cb3bd308d56d0297c7cb845c718693d4cedf7697
uzair354123 <uzairdevil354123@gmail.com>
2022-09-02 21:36:35 +0500

    Fixes

--- src/components/commons/Button.js
+++ src/components/commons/Button.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@mui/material";
 export default function ButtonComponent({ text, link }) {
   return (
     <Link className="btn-primary" to={link}>
-      <Button>{text}</Button>
+      <span>{text}</span>
     </Link>
   );
 }
