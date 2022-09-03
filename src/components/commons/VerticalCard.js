9103aaa81628437d6af56eb36bb77da9ded95f38
uzair354123 <uzairdevil354123@gmail.com>
2022-09-03 22:13:27 +0500

    Vertical Cards Added

---
+++ src/components/commons/VerticalCard.js
@@ -0,0 +1,77 @@
+import React, { useRef } from "react";
+import { Grid } from "@mui/material";
+import img from "../../assets/img/author.webp";
+import img2 from "../../assets/img/thumbnail-01.webp";
+import { Link } from "react-router-dom";
+
+export default function Card() {
+  // use the following ref to underline the paragraph when hovered on card
+  const lineRef = useRef();
+  // use the following ref to scale the image when hovered on card
+  const imgRef = useRef();
+  // use the following ref to make the card active when hovered on card
+  const cardRef = useRef();
+
+  const handleMouseEnter = () => {
+    lineRef.current.style.backgroundSize = "100% 2px";
+    imgRef.current.style.transform = "scale(1.1)";
+    cardRef.current.classList.add("card-active");
+  };
+  const handleMouseLeave = () => {
+    lineRef.current.style.backgroundSize = "0 2px";
+    imgRef.current.style.transform = "none";
+    cardRef.current.classList.remove("card-active");
+  };
+  return (
+    <div
+      className="card-parent"
+      onMouseEnter={() => handleMouseEnter()}
+      onMouseLeave={() => handleMouseLeave()}
+      ref={cardRef}
+    >
+      <Grid
+        container
+        className="vertical-card"
+        display="flex"
+        flexDirection="column"
+        gap={{ xs: 4, md: 4, sm: 4 }}
+      >
+        <Grid
+          item
+          xs={8}
+          md={8}
+          sm={8}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          gap={{ md: 3, xs: 3, sm: 3 }}
+        >
+          <div className="category">
+            <span>Design</span>
+          </div>
+          <div className="desc">
+            <h2>
+              <Link to="/" className="hover-line" ref={lineRef}>
+                Lorem ipsum dolor sit, amet ...
+              </Link>
+            </h2>
+          </div>
+        </Grid>
+        <Grid item xs={12} md={12} sm={12}>
+          <div className="card-img-parent">
+            <Link to="/">
+              <img
+                src={img2}
+                width={250}
+                height={250}
+                alt="Loading..."
+                className="card-img"
+                ref={imgRef}
+              />
+            </Link>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
--- src/components/home/Innovation.js
+++ src/components/home/Innovation.js
@@ -2,13 +2,25 @@ import React from "react";
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
 import Tabs from "../commons/Tabs";
+import VerticalCard from "../commons/VerticalCard";
 export default function Innovation() {
   return (
-    <div className="featured-posts">
+    <div className="innovation">
       <Container sx={{ maxWidth: "1280px !important" }}>
         <h1 className="section-heading">Innovation & Tech</h1>
         <Grid container gap={{ md: 2, sm: 2, xs: 1 }}>
           <Tabs />
+        </Grid>
+        <Grid container columnSpacing={{ md: 2, sm: 2, xs: 1 }}>
+          <Grid item md={4} xs={12} sm={6}>
+            <VerticalCard />
+          </Grid>
+          <Grid item md={4} xs={12} sm={6}>
+            <VerticalCard />
+          </Grid>
+          <Grid item md={4} xs={12} sm={6}>
+            <VerticalCard />
+          </Grid>
         </Grid>
       </Container>
     </div>
