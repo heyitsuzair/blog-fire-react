63ae58cbcb7d3f1ea79c416dc694e4638f584064
uzair354123 <uzairdevil354123@gmail.com>
2022-09-04 03:32:13 +0500

    Improvements

--- src/components/commons/Topic.js
+++ src/components/commons/Topic.js
@@ -1,30 +1,14 @@
-import React, { useRef } from "react";
+import React from "react";
 import img1 from "../../assets/img/thumbnail-02.webp";
 import { Link } from "react-router-dom";
 
 export default function Topic({ title, img }) {
-  // use the following reference to make the image scale when mouse enters h5
-  const ref = useRef();
-
-  const handleMouseEnter = () => {
-    ref.current.style.transform = "scale(1.1)";
-  };
-  const handleMouseLeave = () => {
-    ref.current.style.transform = "";
-  };
-
   return (
     <div className="topic">
       <div className="inner">
-        <Link
-          to="/"
-          className="topic-link"
-          style={{ textDecoration: "none" }}
-          onMouseEnter={() => handleMouseEnter()}
-          onMouseLeave={() => handleMouseLeave()}
-        >
+        <Link to="/" className="topic-link" style={{ textDecoration: "none" }}>
           <div className="img">
-            <img src={img1} alt="Loading..." ref={ref} />
+            <img src={img1} alt="Loading..." />
           </div>
           <h5>{title}</h5>
         </Link>
