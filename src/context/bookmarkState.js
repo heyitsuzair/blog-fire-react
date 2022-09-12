ab14e2101d14b648d2946bd65e060b5380e9f061
heyitsuzair <uzairdevil354123@gmail.com>
2022-09-12 18:00:17 +0500

    Final Push

--- src/context/bookmarkState.js
+++ src/context/bookmarkState.js
@@ -45,7 +45,6 @@ export default function ModeState({ children }) {
 
   useEffect(() => {
     // set the default theme mode got from localStorage mode item
-    console.log(JSON.parse(localStorage.getItem("blog-bookmarks")));
     setBookmarks(JSON.parse(localStorage.getItem("blog-bookmarks")));
     //eslint-disable-next-line
   }, []);
